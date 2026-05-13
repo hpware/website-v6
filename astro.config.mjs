068d6199383eaffbf0686c40586c8b35669e8ce0
Howard <hw@yuanhau.com>
2026-05-14 01:44:34 +0800

    Dynamic pages.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel";
 import { defineConfig, fontProviders } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
@@ -11,6 +12,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: "https://v6.yuanhau.com",
+  adapter: vercel(),
   integrations: [mdx(), sitemap(), react()],
 
   fonts: [
